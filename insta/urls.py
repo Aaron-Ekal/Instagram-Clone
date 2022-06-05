a07f3ccfb38661e6f82f9daa05f967ebe2a22af0
Aaron-Ekal <ekalaaronkalale@gmail.com>
2022-06-06 01:43:23 +0300

    configuring url paths with project

--- insta/urls.py
+++ insta/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('insta_app.urls')),
+    # path('accounts/', include('django_registration.backends.one_step.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('logout', views.logout_then_login, {'login_url': '/'})
 ]

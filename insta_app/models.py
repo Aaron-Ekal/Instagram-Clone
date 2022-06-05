2a58bb1a7c67bdd90d262a169c479e29f636c130
Aaron-Ekal <ekalaaronkalale@gmail.com>
2022-06-06 00:01:09 +0300

    created Profile model

--- insta_app/models.py
+++ insta_app/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+
+class Profile(models.Model):
+    photo = CloudinaryField('image')
+    username = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.CharField(max_length=140, blank=True)
+
+    def __str__(self):
+        return self.username
+
+    def save_profile(self):
+        self.save()
+
+    def update_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    @classmethod
+    def get_profile_by_username(cls, username):
+        profile = cls.objects.filter(username=username)
+        return profile

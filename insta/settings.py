0c2b3c5742907d26f3bcf811aa068b1d2be93b33
Aaron-Ekal <ekalaaronkalale@gmail.com>
2022-06-06 00:20:20 +0300

    configuring timezone and cloudinary

--- insta/settings.py
+++ insta/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'insta_app',
+    'cloudinary',
 ]
 
 MIDDLEWARE = [
@@ -109,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 

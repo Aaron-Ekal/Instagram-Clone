7b5dc3f1f36eb9f08a13b9e6dbb7650662a27500
Aaron-Ekal <ekalaaronkalale@gmail.com>
2022-06-06 00:01:49 +0300

    image model

--- insta_app/models.py
+++ insta_app/models.py
@@ -25,3 +25,47 @@ class Profile(models.Model):
     def get_profile_by_username(cls, username):
         profile = cls.objects.filter(username=username)
         return profile
+
+
+class Image(models.Model):
+    image = CloudinaryField('image')
+    name = models.CharField(max_length=30)
+    caption = models.CharField(max_length=140)
+    profile = models.ForeignKey(
+        User, on_delete=models.CASCADE)
+    total_likes = models.IntegerField(default=0)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="images")
+    total_comments = models.IntegerField(default=0)
+    post_time = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-post_time']
+
+    def __str__(self):
+        return self.name
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    def update_caption(self, new_caption):
+        self.caption = new_caption
+        self.save()
+
+    @classmethod
+    def get_images_by_user(cls, user):
+        images = cls.objects.filter(user=user)
+        return images
+
+    @classmethod
+    def search_by_image_name(cls, search_term):
+        images = cls.objects.filter(name__icontains=search_term)
+        return images
+
+    @classmethod
+    def get_image(cls, id):
+        image = cls.objects.get(id=id)
+        return image
